bc8503e33b5c9c9601a05438e246c60082326a68
EC2 Default User <ec2-user@ip-172-31-47-129.ec2.internal>
2023-11-20 18:16:04 +0000

    k8s deploy test

---
+++ k8s_deployment.sh
@@ -0,0 +1,2 @@
+kubectl create deployment blue-deploy --image=510590228660.dkr.ecr.us-east-1.amazonaws.com/udacity/capstone:1.0.3 --replicates 1 &&
+    kubectl expose deployment/blue-deploy --type="LoadBalacner" --port 8080
