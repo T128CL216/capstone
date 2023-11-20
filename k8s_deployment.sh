f94c663644309837b4d571bedffd216d49f41bbe
EC2 Default User <ec2-user@ip-172-31-47-129.ec2.internal>
2023-11-20 19:34:46 +0000

    commit before checkout

--- k8s_deployment.sh
+++ k8s_deployment.sh
@@ -1,2 +1,2 @@
-kubectl create deployment blue-deploy --image=510590228660.dkr.ecr.us-east-1.amazonaws.com/udacity/capstone:1.0.3 --replicates 1 &&
-    kubectl expose deployment/blue-deploy --type="LoadBalacner" --port 8080
+kubectl create deployment blue-deploy --image=510590228660.dkr.ecr.us-east-1.amazonaws.com/udacity/capstone:1.0.3 --replicas 1 &&
+    kubectl expose deployment/blue-deploy --type="LoadBalancer" --port 8080
